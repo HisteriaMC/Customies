3d23e4fc0526824307d117d6b11b0a9087bc4660
dadodasyra <44753923+dadodasyra@users.noreply.github.com>
2024-09-13 22:22:56 +0200

    Reapply "Merge branch 'refs/heads/v7'"
    This reverts commit e1296df2069ea12b4f146eec4867695f36a8e619.

---
+++ src/block/InterfaceBlock.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace customiesdevs\customies\block;
+
+//This class is used to know if we need to remove block placing against a block
+//This implementation is client side and still needs to
+interface InterfaceBlock{}
